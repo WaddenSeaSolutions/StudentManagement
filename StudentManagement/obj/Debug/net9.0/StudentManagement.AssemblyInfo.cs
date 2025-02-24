f6c5304f4a4a9582df694c7d812ab5baf04a1cfb
Andreas Eriksen <pjuske.10@live.dk>
2025-02-24 10:38:52 +0100

    Yes Sir

--- StudentManagement/obj/Debug/net9.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net9.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29d41572cbe65e020ffc5d4034a06ae46986b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
